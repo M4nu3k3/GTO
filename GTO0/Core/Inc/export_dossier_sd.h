b5c2769ecf490377d7a102ec40acd57fddbd5f7d
ISkyCraftI <133578001+ISkyCraftI@users.noreply.github.com>
2025-05-23 15:11:47 +0200

    ajout2 de gto_reader
    il est sensé être mieux mais j'ai pas testé

---
+++ GTO0/Core/Inc/export_dossier_sd.h
@@ -0,0 +1,37 @@
+/* USER CODE BEGIN Header */
+/**
+  **************************************************************************************************
+  * @file           : export_dossier_sd.h
+  * @brief          : Interface pour l’exportation de tous les fichiers .gto d’un dossier vers un CSV.
+  *
+  * Ce module utilise FatFS pour parcourir un dossier de la carte SD,
+  * lit chaque fichier .gto, et produit un fichier CSV.
+  *
+  * Format final : [ID_Etudiant, ID_QCM, Q1...Q20, Note] + moyenne
+  *
+  * Auteur : Matthieu
+  **************************************************************************************************
+  */
+/* USER CODE END Header */
+
+#ifndef INC_EXPORT_DOSSIER_SD_H
+#define INC_EXPORT_DOSSIER_SD_H
+
+#include "ff.h"           // FatFS
+#include "gto_reader.h"   // traitement d’un fichier .gto
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief Exporte tous les fichiers .gto d’un dossier SD dans un fichier CSV sur la carte SD.
+ * @param nom_dossier nom du dossier à parcourir (ex: "Mathematiques")
+ */
+void export_dossier_sd(const char *nom_dossier);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // INC_EXPORT_DOSSIER_SD_H
---
+++ GTO0/Core/Src/gto_reader.c
@@ -0,0 +1,43 @@
+/* USER CODE BEGIN Header */
+/**
+  *********************************************************************************************************************
+  * @file           : gto_reader.h
+  * @brief          : Déclarations des fonctions de lecture de fichiers .gto et génération d'un fichier CSV.
+  *
+  * Ce module permet de :
+  *   - Lire des fichiers binaires au format .gto
+  *   - Extraire les identifiants et réponses aux questions
+  *   - Générer un fichier CSV à partir d'un dossier contenant ces fichiers
+  *
+  * Auteur : Matthieu
+  *********************************************************************************************************************
+  */
+/* USER CODE END Header */
+#include "gto_reader.h"
+#include "FATFS_SD.h"  // FATFS
+
+int traiter_gto_sd(FIL *file, FIL *csv_file) {
+    UINT bytes_read;
+    char id_etud[4] = {0};
+    char id_qcm[4] = {0};
+    BYTE buffer[2 * MAX_REPONSES];
+
+    f_lseek(file, OFFSET_ID_ETU);
+    f_read(file, id_etud, 3, &bytes_read);
+    f_read(file, id_qcm, 3, &bytes_read);
+
+    f_lseek(file, OFFSET_REPONSES);
+    f_read(file, buffer, 2 * MAX_REPONSES, &bytes_read);
+
+    int note = 0;
+    f_printf(csv_file, "%s%s%s", id_etud, CSV_SEPARATOR, id_qcm);
+    for (int i = 0; i < MAX_REPONSES; i++) {
+        char corr = buffer[2 * i];
+        char etud = buffer[2 * i + 1];
+        if (corr == etud) note++;
+        f_printf(csv_file, "%s%c", CSV_SEPARATOR, etud);
+    }
+    f_printf(csv_file, "%s%d\n", CSV_SEPARATOR, note);
+    return note;
+}
+
